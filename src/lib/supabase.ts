c90f5aaf19f722c2eae389476d33e1dc1612065e
Shashinth <shashinthlive@gmail.com>
2026-09-25 21:13:54 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,6 +5,7 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    persistSession: false,
     lock: false,
   },
 });
